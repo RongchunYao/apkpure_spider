2823ab44f7b54540c078c798b9efb284640a5fce
rongchunyao <15895873878@163.com>
2018-10-31 19:39:28 +0800

    second commit

--- README.md
+++
@@ -1,8 +0,0 @@
-please first take a look at my_conf.py
-this tool will not duplicately download apps
-$[keywd].txt: apps that successfully downloaded
-app.txt: all apps name 
-there are two places u can modify in apkpure.py: first is page of apps that u download.U can comment it to download all apps. (I can not afford so much time to download all pages of apps just to test). And the second is timeout of downloading apps, with defaulted value 300
-
-another interesting thing:
-	the website only have 14 apps per page, but the website take it as 15
--- my_conf.py
+++ my_conf.py
@@ -10,7 +10,7 @@ my_headers={
 "cache-control": "max-age=0",
 "upgrade-insecure-requests": "1"
 }
-#make sure to change cookie to your cookie
+#make sure to change cookie to your cookie or just comment it
 my_headers["cookie"]="__cfduid=dc4f6977be31cb83f064419310ded65661540542077; _ga=GA1.2.1950587501.1540542078; _gid=GA1.2.1198378188.1540894083; apkpure__lang=en; AMP_TOKEN=%24NOT_FOUND; __atuvc=26%7C43%2C37%7C44; __atuvs=5bd94f5b603b2944002"
 my_headers["user-agent"]="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 
